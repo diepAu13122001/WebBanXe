ab35329bba70f7b95558e58191f457d3d66f0c66
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2022-08-08 23:22:11 +0700

    create java web project

--- HDD/src/main/webapp/js/nav.js
+++ HDD/src/main/webapp/js/nav.js
@@ -18,12 +18,12 @@ function setAdNav() {
 function setNav() {
     document.querySelector('.navbar').innerHTML = '<a href="../index.html"> <img src="../img/dark-logo.png" class="brand-logo" alt=""></a>\n' +
         '        <ul class="links-container">\n' +
-        '            <li class="link-item"><a href="../html/cus_products.html" class="link">Sản phẩm</a></li>\n' +
+        '            <li class="link-item"><a href="helloServlet" class="link">Sản phẩm</a></li>\n' +
         '            <li class="link-item"><a href="../html/cus-insurance.html" class="link">Chính sách bảo hành</a></li>\n' +
         '            <li class="link-item"><a href="../html/cus_aboutUs.html" class="link">Về chúng tôi</a></li>\n' +
         '        </ul>\n' +
         '        <div class="nav-items">\n' +
-        '            <a>\n' +
+        '            <a onclick="showSearchInput()">\n' +
         '               <img id="search" src="https://img.icons8.com/ios-filled/60/000000/search--v1.png"/>\n' +
         '            </a>\n' +
         '            <a>\n' +
@@ -34,4 +34,8 @@ function setNav() {
         '                <img src="https://img.icons8.com/ios-glyphs/100/000000/shopping-cart--v1.png"/>\n' +
         '            </a>\n' +
         '        </div>';
+}
+
+function showSearchInput() {
+    
 }
