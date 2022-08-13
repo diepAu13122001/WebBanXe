1e021e5d67432c8e89103e9f458cf2242ae310c3
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2022-08-14 00:36:38 +0700

    Update Diep_function_CRUD.sql

--- Diep_function_CRUD.sql
+++ Diep_function_CRUD.sql
@@ -1,6 +1,12 @@
 -- home.jsp --
---1. get the product has the most buys with the minialist price--
-
+--1. get the product has the most buys--
+SELECT * FROM product p JOIN
+(SELECT prd_id, sum(dtl_productAmount) as qty
+FROM bill_detail
+GROUP BY prd_id
+ORDER by qty desc
+LIMIT 1) table1
+ON p.prd_id = table1.prd_id
 
 
 --2. Get 8 lastest product--
