e158e899eb1fdbf9721dbdb7f330fb52017e41b9
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2022-08-13 23:48:04 +0700

    Update Diep_function_CRUD.sql

--- Diep_function_CRUD.sql
+++ Diep_function_CRUD.sql
@@ -1,6 +1,12 @@
 -- home.jsp --
--- 1. Get 8 lastest product--
-SELECT * from product_upload
+--1. get the product has the most buys with the minialist price--
+
+
+
+--2. Get 8 lastest product--
+SELECT * FROM product p JOIN 
+(SELECT * from product_upload
 WHERE upl_status IN ("Sắp mở (chỉ cho đặt cọc trước)", "Đang bán")
 ORDER BY upl_uploadedDate DESC
-LIMIT 8
+LIMIT 8) table1
+ON p.prd_id = table1.prd_id
