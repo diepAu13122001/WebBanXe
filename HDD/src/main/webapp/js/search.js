e276658350caaa141a26c90917bd1e9cb20337c5
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2022-08-15 16:26:44 +0700

    Delete HDD directory

--- HDD/src/main/webapp/js/search.js
+++
@@ -1,6 +0,0 @@
-// const searchPanElement = document.querySelector('#search-key');
-// const searchKey = searchBox.value;
-//
-// console.log(searchKey);
-// searchPanElement.innerHTML = searchKey;
-//
